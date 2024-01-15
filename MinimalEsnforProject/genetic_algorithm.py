5b9197d47d85cc7604a1b38dacfc6c6077cbb162
Qqwer711 <95687313+Qqwer711@users.noreply.github.com>
2024-01-15 19:50:22 +0700

    Add files via upload
    1/15/2024

---
+++ MinimalEsnforProject/genetic_algorithm.py
@@ -0,0 +1,9 @@
+
+class GenaticAlgorithm:
+    @staticmethod
+    def resSize():
+        Num = input("Enter a resSize: ")
+        return Num
+    def MSE(self,Num):
+        Rmse = Num
+        print("recived MSE from minimalESN: mse = ",Rmse)
